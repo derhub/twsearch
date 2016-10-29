e8bebaf2fc8de21e4ce0301efb857d340107bb9b
der <Johnder baul>
2016-10-29 22:34:56 +0800

    add default blade layout

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Twitter Cty</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" media="screen" title="no title">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>

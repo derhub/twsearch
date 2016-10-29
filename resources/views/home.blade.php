72221c6e0b394d67c1be479b0edcd9f5113235d9
der <Johnder baul>
2016-10-29 22:37:24 +0800

    add home tempalte

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.default')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="inner-wrapper">
+            <div class="map-content">
+                
+            </div>
+            <div class="search-bar">
+                <form class="form-inline" action="={{ route('search.tweets') }}" method="post">
+                    <input class="form-control input-search" type="search" name="search" placeholder="City name" value="">
+                    <input type="submit" class="btn btn-primary search-button" value="search">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
